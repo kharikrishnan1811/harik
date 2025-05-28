076ab1739ee57065b85cf45efeb605f93f6b17c1
hari <kharikrishnan1811@gmail.com>
2025-05-28 16:19:22 +0530

    fit the toggle button in navbar

--- proto.js
+++ proto.js
@@ -10,3 +10,12 @@ function toggleContent() {
     button.textContent = "Read More";
   }
 }
+
+
+  const menuToggle = document.getElementById("menu-toggle");
+  const sidebar = document.getElementById("sidebar");
+
+  menuToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+  });
